060a0fd601d1dbc957705ab44e67ec1901ed3614
Binh Nguyen <86753146+BinhBender@users.noreply.github.com>
2024-06-04 10:57:09 -0700

    Further implement inventory

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -7,7 +7,34 @@ const JUMP_VELOCITY = -400.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+@onready var inv: Inventory = preload("res://scripts/inventory/Inventory .tres")
+@onready var pickupArea: Area2D = get_node("Area2D")
 
+func _ready():
+	pickupArea.collision_mask = 2
+
+func GetCloser(a: Node2D, b: Node2D) -> bool:
+	return global_position.distance_squared_to(a.global_position) < global_position.distance_squared_to(b.global_position)
+
+
+func GetClosestBody() -> Node2D:
+	var bodies = pickupArea.get_overlapping_bodies()
+	if bodies.size() == 0:
+		return null
+	bodies.sort_custom(GetCloser)
+	return bodies[0]
+	
+
+
+func _process(delta) -> void:
+#Pickup
+	if Input.is_action_pressed("interact"):
+		var body = GetClosestBody()
+		if body != null:
+			#TODO add stuff here
+			inv.AddItem(body.)#NOT CORRECT ARGUMENT
+			pass
+		
 func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
--- scripts/inventory/InventoryItem.gd
+++ scripts/inventory/InventoryItem.gd
@@ -6,3 +6,6 @@ class_name InvItem
 @export var texture: Texture2D 
 @export var count: int = 0
 @export var max_count: int
+
+func effect():
+	pass
