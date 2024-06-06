f4c8e98edd8597f1f179c558a18670b2ac0fc8e5
Connor <cbegley027@gmail.com>
2024-06-05 23:00:32 -0400

    add barebones starting menu

---
+++ scripts/Menu.gd
@@ -0,0 +1,7 @@
+extends Control
+
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://scenes/main.tscn")  #  Starts game
+
+func _on_quit_pressed():
+	get_tree().quit()  # Closes game
