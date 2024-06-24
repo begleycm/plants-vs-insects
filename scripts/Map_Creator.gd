ee62ab3196ffa70074eedf8bf5a8fcedb761db60
Connor <cbegley027@gmail.com>
2024-06-24 02:46:43 -0400

    add path creation

--- scripts/Map_Creator.gd
+++ scripts/Map_Creator.gd
@@ -3,80 +3,53 @@ extends Node2D
 @export var map_length:int = 32
 @export var map_height:int = 18
 
-@export var start = [-(map_length / 2) + 1, -(map_height / 2)]
-@export var end = [(map_length / 2) - 2, (map_height / 2) - 1]
+@export var start = [-(map_length / 2), -(map_height / 2)]
+@export var end = [(map_length / 2) - 1, (map_height / 2) - 1]
 
-@onready var start_place = get_node("start_place")
+var start_mark:Marker2D
+
 var tile_map:TileMap
 
-# Called by main to make the map
-func create_map(map):
+func create_map(map, start_m):
 	tile_map = map
+	start_mark = start_m
 	
 	display_grass()
-	
-	spawn_start()
 
-	#Enables navigation for layer 0
 	tile_map.set_layer_navigation_enabled(0, true)
 	
-	
-	#Random number generator that is used to place blocks
 	var rng = RandomNumberGenerator.new()
 	spawn_blocks(rng)
 	
-	#Places test blocks
-	tile_map.set_cell(0, Vector2i(start[0], start[1]), 0, (Vector2i(3, 1)))
-	tile_map.set_cell(0, Vector2i(end[0], end[1]), 0, (Vector2i(3, 1)))
+	tile_map.set_cell(0, Vector2i(start[0], start[1]), 0, Vector2i(3, 1))
+	tile_map.set_cell(0, Vector2i(end[0], end[1]), 0, Vector2i(3, 1))
 	
-func display_grass():
-	# Places grass in the length x height(ex. 16x9)
-	for n in range (-map_length / 2, map_length / 2):
-		for i in range (-map_height / 2, map_height / 2):
-			tile_map.set_cell(0, Vector2i(n, i), 0, (Vector2i(3, 0)))
+	spawn_start()
 
-'''
-This function spawns in blocks at random across the map. It places
-1 block for every 10 on the map.
-'''
+func display_grass():
+	for n in range(-map_length / 2, map_length / 2):
+		for i in range(-map_height / 2, map_height / 2):
+			tile_map.set_cell(0, Vector2i(n, i), 0, Vector2i(3, 0))
+
 func spawn_blocks(rng:RandomNumberGenerator):
-	var amount_blocks = (map_height * map_length) / 10
+	var amount_blocks:int = (map_height * map_length) / 4
 	
-	for i in range (0, amount_blocks):
-		# This places a block at random across the map
+	for i in range(0, amount_blocks):
 		var rand_l:int = rng.randf_range((-map_length / 2) - 1, map_length / 2)
 		var rand_h:int = rng.randf_range((-map_height / 2) - 1, map_height / 2)
 		
-		'''
-		This checks to see if the block placed will block the exit. If so, 
-		it cannot be placed
-		'''
-		if is_placeable(rand_l, rand_h):
-			tile_map.set_cell(0, Vector2i(rand_l, rand_h), 0, (Vector2i(6, 2)))
-		
-func is_placeable(rand_l:int, rand_h:int):
-	var can_place:bool = true
-	
-	var path_alg = PathAlg.new(tile_map, start, end)
-	# If you can't reach the end after placing this block, don't allow it
-	if !path_alg.can_reach_end():
-		can_place = false
-	
-	return can_place
-	
+		tile_map.set_cell(0, Vector2i(rand_l, rand_h), 0, Vector2i(6, 2))
+			
 func spawn_start():
-	var start_sprite = Sprite2D.new()
-	start_sprite.texture = load("res://assets/darktile.png")
-	start_sprite.position = Vector2(start[0], start[1])
+	start_mark.position = Vector2i(start[0] * 16, start[1] * 16)
+
+func is_end_reachable():
+	var nav_test = NavigationAgent2D.new()
+	var vec = Vector2(end[0] * 16, end[1] * 16)
+	nav_test.target_position = vec
 	
-	# Explicitly get the node
-	var target_node = get_node("res://scenes/main.tscn")
-	#print(target_node.get_path())
+	start_mark.add_child(nav_test)
 	
-	# Check if the node is valid
-	if target_node:
-		print("Adding sprite...")
-		target_node.add_child(start_sprite)
-		print("Sprite addded!")
-	else:
-		print("Error: Target node is null or invalid")
+	nav_test.debug_enabled = true
+	
+	return nav_test.is_target_reachable()
--- scripts/main.gd
+++ scripts/main.gd
@@ -1,11 +1,20 @@
 extends Node
 
-@onready var tile_map = $Nav_Mesh/TileMap
+@onready var tile_map = $TileMap
+@onready var start_mark = $start_m
 
 const map = preload("res://scripts/Map_Creator.gd")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var map_creator = map.new()
-	# Creates the map
-	map_creator.create_map(tile_map)
+	# Creates the map, recursively makes it until a path to the end is made
+	while true:
+		map_creator.create_map(tile_map, start_mark)
+	
+		await get_tree().create_timer(0.05).timeout
+	
+		print(map_creator.is_end_reachable())
+		if map_creator.is_end_reachable():
+			break
+			
