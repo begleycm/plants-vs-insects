95a961fa0339c783646434a4ec20dc4ea6fc5477
Connor <cbegley027@gmail.com>
2024-06-25 00:24:09 -0400

    refactor Map_Creator

--- scripts/Map_Creator.gd
+++ scripts/Map_Creator.gd
@@ -1,37 +1,56 @@
 extends Node2D
+class_name Map_Creator
 
 @export var map_length:int = 32
 @export var map_height:int = 18
 
-@export var start = [-(map_length / 2), -(map_height / 2)]
-@export var end = [(map_length / 2) - 1, (map_height / 2) - 1]
+# Start and end locations for mobs
+@export var start:Array = [-(map_length / 2), -(map_height / 2)]
+@export var end:Array = [(map_length / 2) - 1, (map_height / 2) - 1]
 
-var start_mark:Marker2D
+# Defined here so we only have one nav at a time
+var map_nav:NavigationAgent2D = NavigationAgent2D.new()
 
+# Tile map to use for placing blocks in code
 var tile_map:TileMap
 
-func create_map(map, start_m):
-	tile_map = map
-	start_mark = start_m
+# Start marker for where mobs spawn
+var start_mark:Marker2D
+
+# Constructor
+func _init(tile_map:TileMap = null, start_mark:Marker2D = null):
+	self.tile_map = tile_map
+	self.start_mark = start_mark
 	
+# Called when the node enters the scene tree
+func _ready():
+	create_map()
+
+func create_map() -> void:
+	# Creates the base map of grass using length X width
 	display_grass()
 
+	# Layer 0 of the map has nav enabled, while others don't
 	tile_map.set_layer_navigation_enabled(0, true)
 	
-	var rng = RandomNumberGenerator.new()
+	# Creates a random number and spawns blocks using that
+	var rng:RandomNumberGenerator = RandomNumberGenerator.new()
 	spawn_blocks(rng)
 	
+	# Sets the start and end block to a dirt block for visual aid
 	tile_map.set_cell(0, Vector2i(start[0], start[1]), 0, Vector2i(3, 1))
 	tile_map.set_cell(0, Vector2i(end[0], end[1]), 0, Vector2i(3, 1))
 	
+	# Spawns the start marker
 	spawn_start()
 
-func display_grass():
+func display_grass() -> void:
 	for n in range(-map_length / 2, map_length / 2):
 		for i in range(-map_height / 2, map_height / 2):
 			tile_map.set_cell(0, Vector2i(n, i), 0, Vector2i(3, 0))
 
-func spawn_blocks(rng:RandomNumberGenerator):
+func spawn_blocks(rng:RandomNumberGenerator) -> void:
+	# Spawns blocks equal to the grass divided by 4
 	var amount_blocks:int = (map_height * map_length) / 4
 	
 	for i in range(0, amount_blocks):
@@ -40,16 +59,15 @@ func spawn_blocks(rng:RandomNumberGenerator):
 		
 		tile_map.set_cell(0, Vector2i(rand_l, rand_h), 0, Vector2i(6, 2))
 			
-func spawn_start():
+func spawn_start() -> void:
 	start_mark.position = Vector2i(start[0] * 16, start[1] * 16)
 
-func is_end_reachable():
-	var nav_test = NavigationAgent2D.new()
-	var vec = Vector2(end[0] * 16, end[1] * 16)
-	nav_test.target_position = vec
+func is_end_reachable() -> bool:
+	var vec:Vector2 = Vector2(end[0] * 16, end[1] * 16)
+	map_nav.target_position = vec
 	
-	start_mark.add_child(nav_test)
+	start_mark.add_child(map_nav)
 	
-	nav_test.debug_enabled = true
+	map_nav.debug_enabled = true
 	
-	return nav_test.is_target_reachable()
+	return map_nav.is_target_reachable()
--- scripts/main.gd
+++ scripts/main.gd
@@ -3,18 +3,18 @@ extends Node
 @onready var tile_map = $TileMap
 @onready var start_mark = $start_m
 
-const map = preload("res://scripts/Map_Creator.gd")
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var map_creator = map.new()
+	var map = Map_Creator.new(tile_map, start_mark)
+	
 	# Creates the map, recursively makes it until a path to the end is made
 	while true:
-		map_creator.create_map(tile_map, start_mark)
+		map.create_map()
 	
-		await get_tree().create_timer(0.05).timeout
-	
-		print(map_creator.is_end_reachable())
-		if map_creator.is_end_reachable():
+		await get_tree().create_timer(0.1).timeout
+		print(map.is_end_reachable())
+		
+		# If path is made, stop trying to make a new one
+		if map.is_end_reachable():
 			break
 			
